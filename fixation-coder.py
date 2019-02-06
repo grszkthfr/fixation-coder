ea22484940e639be7a06f81fe15a34efd7c98426
Jonas Großekathöfer <grszkthfr@outlook.com>
2019-02-06 09:11:43 +0100

    start splitting function

--- fixation-coder.py
+++ fixation-coder.py
@@ -1,45 +1,176 @@
 #!/usr/bin/python
 
-# import sys
-# import os
+import sys
+from os import path
 import csv
 import cv2
 # import math
 # import numpy as np
 
-# directories
-# LOG_FILE = "subject_01/log_2019-01-09_" + sys.argv[1] + ".txt"
-LOG_FILE = "log/subject_01/log_2019-01-25_01_1.9_1.txt"
-# FRAME_DIR = "subject_01/frames/" + sys.argv[1] + "/"
-FRAME_DIR = "log/subject_01/frames/1.9_1/"  
-OUT_DIR = "data/subject_01/"
+#APP
+# from PyQt5.QtCore import *
+# from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDialog,QLineEdit, QMenu # QPushButton, QLineEdit
 
-with open(LOG_FILE, 'r') as f:
-    READER = csv.reader(f, delimiter='\t')
-    ALL_FRAMES = list(READER)
-    # print(ALL_FRAMES[:2])
-    for row in ALL_FRAMES[1:]:
-        if (int(row[3]) % 10 == 0 and int(row[3]) >= 230):
-            # print(row)
-            print(row[3] + ".ppm\n" + row[4] + "," + row[5])
-            frame = FRAME_DIR + row[2] + "_" + "frame_" + row[3] + ".ppm"
-            print(frame)
-            img = cv2.imread(frame)
 
-            screenPos = (int(float(row[4])/2), int(float(row[5])/2))  # 216
+LOG_FILE = "log_2019-01-25_01_val_1_1.txt"
 
-            cv2.circle(img, screenPos, 1, (0, 255, 255), 4)
+# OUT_DIR = "data/subject_01/"
 
-            img = cv2.flip(img, 0)  # 0 = horizontal, 1 = vertical, -1 = both
+def write_line(OUTPUT_DIR, INPUT):
 
-            while 1:
-                cv2.imshow("window", img)
+    # check if file and folder already exist
+    makeDirectory('log_files')
+    fileName = 'log_files' + os.path.sep + exp_name + '_' + getDate() + '_' + session_info['subject'].zfill(2) + '.csv' 
 
-                k = cv2.waitKey(33)
-                if k == 27:    # Esc key to stop
-                    break
-                elif k == -1:  # normally -1 returned,so don't print it
-                    continue
-                else:
-                    print(repr(chr(k % 256)))
-                    break  # else print its value
+    # generate file name with name of the experiment and subject
+    # open file
+    # 'a' = append; 'w' = writing; 'b' = in binary mode
+    with open(fileName, 'ab') as saveFile:
+        fileWriter = csv.writer(saveFile, delimiter='\t') # tab separated
+        if os.stat(fileName).st_size == 0:  # if file is empty, insert header
+            fileWriter.writerow(('INPUT'))
+
+        # write trial
+        fileWriter.writerow(INPUT)
+
+
+def draw_fixation():
+    print("Draw Fixation from log of frame into img of frame")
+
+
+def get_frames_img():
+    print("Handled img of frame")
+
+
+def get_frames_log():
+    print("Handled log of frame")
+
+
+def drawFixation(log_file, frame_directory = "frames"):
+
+    # extract relevant information from log filename
+    SUBJECT_ID = "subject_" + str.split(log_file, "_")[2]
+    VIDEO_ID = log_file[18:-4]
+    LOG_FILE = path.join("log", SUBJECT_ID, log_file)
+    LOG_FILE = path.abspath(LOG_FILE)
+
+    # directory of frames
+    FRAME_DIR = path.join("log", SUBJECT_ID, "frames", VIDEO_ID)
+    FRAME_DIR = path.abspath(FRAME_DIR)
+
+    with open(LOG_FILE, 'r') as f:
+
+        READER = csv.reader(f, delimiter='\t')
+        ALL_FRAMES = list(READER)
+
+        # print(ALL_FRAMES[:2])
+        for row in ALL_FRAMES[1:]:
+
+            if (int(row[3]) % 10 == 0 and int(row[3]) >= 230):
+
+                # print(row[3] + ".ppm\n" + row[4] + "," + row[5])
+
+                FRAME_FILE = row[2] + "_" + "frame_" + row[3] + ".ppm"
+                FRAME = path.join(FRAME_DIR, FRAME_FILE)
+
+                img = cv2.imread(FRAME)
+
+                screenPos = (int(float(row[4])/2), int(float(row[5])/2))  # 216
+
+                cv2.circle(img, screenPos, 3, (0, 255, 255), 2)
+
+                img = cv2.flip(img, 0)  # 0 = horizontal, 1 = vertical, -1 = both
+
+                cv2.imshow("frame", img)
+
+                # k = cv2.waitKey(33)
+
+                # if k == 27:    # Esc key to stop
+                #     break
+
+                # elif k == -1:  # normally -1 returned,so don't print it
+                #     continue
+                # else:
+                #     print(repr(chr(k % 256)))
+                #     break  # else print its value
+
+                next_frame = True
+                while next_frame:
+
+                    cv2.imshow("frame", img)
+
+                    k = cv2.waitKey(33)
+
+                    if k == 27:    # Esc key to stop
+                        next_frame = False
+                        break
+
+                    elif k == -1:  # normally -1 returned, so don't print it
+                        continue
+
+                    elif k == 49: # #1
+                        fixation = "person"
+                        break
+
+                    elif k == 50:  # #2
+                        fixation = "object"
+                        break
+
+                    else:
+                        print(repr(chr(k % 256)))
+                        print()
+                        break  # else print its value
+
+                print(fixation)
+                if next_frame == False: break
+                
+
+# def window():
+#     app = QApplication(sys.argv)
+#     win = QWidget()
+#     button = QPushButton(win)
+#     button.setText("Hello World!")
+#     button.move(50, 50)
+#     button.clicked.connect(showdialog)
+
+#     # textbox
+#     textbox = QLineEdit(win)
+#     textbox.setDragEnabled(True)
+#     textbox.setText("subject_")
+#     textbox.setToolTip(
+#         "Please enter subject id. Don't forget trailing 0 for id's < 9")
+#     textbox.move(20, 20)
+#     textbox.resize(280, 40)
+
+#     # dropdown
+#     menu = QMenu()
+    
+#     sub_menu = QMenu("Sub Menu")
+#     for i in ["a", "b", "c"]: #or your dict
+#         sub_menu.addAction(i) #it is just a regular QMenu
+
+#     win.addMenu(sub_menu)
+
+#     win.setWindowTitle("Fixation Coder App")
+#     win.show()
+#     sys.exit(app.exec_())
+
+
+# # # Create textbox
+# # self.textbox = QLineEdit(self)
+# # self.textbox.move(20, 20)
+# # self.textbox.resize(280,40)
+
+# # # Create a button in the window
+# # self.button = QPushButton('Show text', self)
+# # self.button.move(20,80)
+
+# # # connect button to function on_click
+# # self.button.clicked.connect(self.on_click)
+# # self.show()
+
+
+drawFixation(log_file=LOG_FILE)
+
+
+# drawFixation(LOG_FILE, FRAME_DIR)
