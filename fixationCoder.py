e7e95fc75a3d1437923e712744045f94c0f6c862
Jonas Großekathöfer <grszkthfr@outlook.com>
2019-02-14 15:11:04 +0100

    add closing windwos when exiting

--- fixationCoder.py
+++ fixationCoder.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
-
-import sys
+# by Jonas Großekathöfer, 2019
+"""
+Module doctring
+TODO: PyLint improvements
+"""
 from os import path
 import os
 import csv
@@ -12,16 +15,16 @@ import cv2
 
 # log file
 LOG_FILE = 'log_2019-01-28_04_1.22.2_2.txt'
-#LOG_FILE = 'log_2019-01-25_01_val_1_1.txt'
+# LOG_FILE = 'log_2019-01-25_01_val_1_1.txt'
 
 # keys
-key_fix_person = 49         # key "1" to code a fixation_id on a person 
-key_fix_object = 50         # key "2" to code a fixation_id on a object
-key_fix_person_absent = 51  # key "3" to code a fixation_id w/o person in scene
-key_no_fixation = 48        # key "0" to code a fixation_id on a person
-key_delete_frame = 100      # key "d" to delete previuos line, go to previous frame
-key_next_frame = 32         # key "space" to write line, go to next frame
-key_exit = 27
+KEY_FIX_PERSON = 49         # key "1" to code a fixation_id on a person
+KEY_FIX_OBJECT = 50         # key "2" to code a fixation_id on a object
+KEY_FIX_PERSON_ABSENT = 51  # key "3" to code a fixation_id w/o person in scene
+KEY_NO_FIXATION = 48        # key "0" to code a fixation_id on a person
+KEY_DELETE_FRAME = 100      # key "d" to delete previuos frame,. go back
+KEY_NEXT_FRAME = 32         # key "space" to write line, go to next frame
+KEY_EXIT = 27
 
 #############################################################################
 
@@ -30,51 +33,56 @@ def writeLine(out_file, subject_id, video_id, frame_id, fixation_id):
 
     fixation_dummy = fixationDummyCoding(fixation_id)
 
-    # generate file name with name of the experiment and subject
-    # open file
-    # 'a' = append; 'w' = writing; 'b' = in binary mode
-    with open(out_file, 'a',newline='') as save_file:
-        writer = csv.writer(save_file, delimiter='\t') # tab separated
+    with open(out_file, 'a', newline='') as save_file:
+        writer = csv.writer(save_file, delimiter='\t')  # tab separated
         if os.stat(out_file).st_size == 0:  # if file is empty, insert header
-            writer.writerow(
-                ('subject_id', 'video_id', 'frame_id',
+            writer.writerow((
+                'subject_id', 'video_id', 'frame_id',
                 'fixation_id', 'fix_person', 'fix_object',
                 'fix_person_absent', 'no_fixation'))
 
         # write trial
-        #print('WRITE (regular):\n', [int(str.split(subject_id, '_')[1]), video_id, frame_id, fixation_id, fixation_dummy[0], fixation_dummy[1], fixation_dummy[2], fixation_dummy[3]])
-        writer.writerow(
-            [int(str.split(subject_id, '_')[1]), video_id, frame_id,
+        # print(
+        #     'WRITE (regular):\n',
+        #     [int(str.split(subject_id, '_')[1]), video_id, frame_id,
+        #     fixation_id, fixation_dummy[0], fixation_dummy[1],
+        #     fixation_dummy[2], fixation_dummy[3]])
+
+        # TODO why brackets?
+        writer.writerow([
+            int(str.split(subject_id, '_')[1]), video_id, frame_id,
             fixation_id, fixation_dummy[0], fixation_dummy[1],
             fixation_dummy[2], fixation_dummy[3]])
 
-    #save_file.close() # needed? to safely delete rows
+    # save_file.close() # needed? to safely delete rows
 
 
 def deleteLine(out_file):
-
+    """
+    Funciton doctring
+    """
     with open(out_file, 'r') as save_file:
 
         reader = csv.reader(save_file, delimiter='\t')
 
         new_save_file = list(reader)
-        #print('OLD:\n', new_save_file)
-        #print('DELETE:\n', new_save_file[-1]) # deleted
+        # print('OLD:\n', new_save_file)
+        # print('DELETE:\n', new_save_file[-1])  # deleted
 
-        new_save_file = new_save_file[:-1] # truncat last entry
-        #print('NEW:\n', new_save_file)
+        new_save_file = new_save_file[:-1]  # truncat last entry
+        # print('NEW:\n', new_save_file)
 
     # write trials, without deleted one
     with open(out_file, 'w', newline='') as save_file:
-        writer = csv.writer(save_file, delimiter='\t') # tab separated
+        writer = csv.writer(save_file, delimiter='\t')  # tab separated
 
-        #print('WRITE (deleted):\n', [row for row in new_save_file])
+        # print('WRITE (deleted):\n', [row for row in new_save_file])
         for row in new_save_file:
             writer.writerow(row)
 
 
 def fixationDummyCoding(fixation_id):
-
+    # TODO why brackets?
     if fixation_id == 'person':
         fixation_dummy = [1,0,0,0]
 
@@ -103,7 +111,7 @@ def readFrames(LOG_FILE):
         READER = csv.reader(f, delimiter='\t')
         all_frames = list(READER)
         frames = onlyScreenshotFrames(all_frames)
-        #print(frames[:5])
+        # print(frames[:5])
 
     return(frames)
 
@@ -114,35 +122,47 @@ def onlyScreenshotFrames(ALL_FRAMES):
     for row in ALL_FRAMES[1:]:      # except header
         if (int(row[3]) % 10 == 0 and int(row[3]) >= 230):
 
-            #print(row)
+            # print(row)
             screenshot_frames.append(row)
 
-    #print(screenshot_frames)
+    # print(screenshot_frames)
     return(screenshot_frames)
 
 
 # TODO saving screenshots
-# def showScreenshotWithFixation(FRAME):
+def showScreenshotWithFixation(FRAME):
+    print("TODO")
 
 
 # TODO handle keys
-#def handleInput(NEXT_FRAME):
+def handleInput(NEXT_FRAME):
+    print("TODO")
+
 
 def updateImageInformation(image, frame_id, fixation_id):
-
 
     font = cv2.FONT_HERSHEY_PLAIN
     height, width, channels = image.shape
 
     # frame information box
-    cv2.rectangle(image,(0, height-50), (int(width/2), height),(0,0,0),-1)
+    cv2.rectangle(
+        image,
+        (0, height-50), (int(width/2), height),
+        (0,0,0), -1)
 
     # frame_id
-    cv2.putText(image, 'frame_id: ' + frame_id, (0,height-25), font, 1, (255,255,255))
+    cv2.putText(
+        image,
+        'frame_id: ' + frame_id, (0,height-25),
+        font, 1, (255,255,255))
 
     # fixtiaon_id
-    cv2.putText(image, 'current fixation_id: ' + fixation_id, (0,height-12), font, 1, (255,255,255))
+    cv2.putText(
+        image,
+        'current fixation_id: ' + fixation_id, (0,height-12),
+        font, 1, (255,255,255))
 
+    # update window
     cv2.imshow('frame', image)
 
 
@@ -164,14 +184,15 @@ def drawFixation(log_file, frame_directory='frames'):
 
     # check if file and folder already exist
     if not path.isdir(out_dir):
-        os.makedirs(out_dir) # throws an error when failing
+        os.makedirs(out_dir)  # throws an error when failing
 
-    out_file = path.join(out_dir,
-        str.split(subject_id, '_')[1] + '_' + video_id + '.csv')
-    print(out_file)
+    out_file = path.join(
+        out_dir, str.split(subject_id, '_')[1] + '_' + video_id + '.csv')
+    # print(out_file)
 
     frames = readFrames(log_file)
-    #print(frames[:5])
+    # print(frames[:5])
+    fixation_id = ''
 
     counter = 0
     while counter < len(frames):
@@ -180,7 +201,6 @@ def drawFixation(log_file, frame_directory='frames'):
 
         video_id = frame[2]
         frame_id = frame[3]
-        fixation_id = ''
 
         frame_screenshot = video_id + '_' + 'frame_' + frame_id + '.ppm'
         print('current frame:\t\t', frame_screenshot)
@@ -193,14 +213,13 @@ def drawFixation(log_file, frame_directory='frames'):
 
         cv2.circle(img, screenPos, 3, (0, 255, 255), 2)
 
-
         img = cv2.flip(img, 0)  # 0 = horizontal, 1 = vertical, -1 = both
 
         updateImageInformation(img, frame_id, fixation_id)
 
         cv2.imshow('frame', img)
 
-        #return(img)
+        # return(img)
 
         next_frame = True
         while next_frame:
@@ -209,37 +228,41 @@ def drawFixation(log_file, frame_directory='frames'):
 
             k = cv2.waitKey(33)
 
-            if k == key_exit:  # Esc
+            if k == KEY_EXIT:  # Esc
+
                 next_frame = False
+                cv2.destroyAllWindows()
                 break
 
             elif k == -1:  # normally -1 returned, so don't print it
                 continue
 
-            elif k == key_fix_person:  # #1
+            elif k == KEY_FIX_PERSON:  # #1
                 fixation_id = 'person'
                 updateImageInformation(img, frame_id, fixation_id)
 
-            elif k == key_fix_object:  # #2
+            elif k == KEY_FIX_OBJECT:  # #2
                 fixation_id = 'object'
                 updateImageInformation(img, frame_id, fixation_id)
 
-            elif k == key_fix_person_absent:  # #3
+            elif k == KEY_FIX_PERSON_ABSENT:  # #3
                 fixation_id = 'person_absent'
                 updateImageInformation(img, frame_id, fixation_id)
 
-            elif k == key_no_fixation:  # #0
+            elif k == KEY_NO_FIXATION:  # #0
                 fixation_id = 'no_fixation'
                 updateImageInformation(img, frame_id, fixation_id)
 
-            elif k == key_delete_frame: # d
+            elif k == KEY_DELETE_FRAME:  # d
                 fixation_id = 'deleted'
 
-            elif k == key_next_frame and fixation_id != 'unknown': # space
+            # TODO make a "valid_code = TRUE"
+            elif k == KEY_NEXT_FRAME and fixation_id != 'unknown' and fixation_id != '':  # space
 
                 if fixation_id != 'deleted':
                     counter += 1
-                    writeLine(out_file, subject_id, video_id, frame_id, fixation_id)
+                    writeLine(
+                        out_file, subject_id, video_id, frame_id, fixation_id)
 
                 elif fixation_id == 'deleted':
                     counter -= 1
@@ -247,17 +270,18 @@ def drawFixation(log_file, frame_directory='frames'):
 
                 break
 
-            else: # else print key value
+            else:  # else print key value
                 fixation_id = 'unknown'
                 print(k)
-                # print(repr(chr(k % 256))) # https://stackoverflow.com/questions/14494101/using-other-keys-for-the-waitkey-function-of-opencv
-                #break
+                # print(repr(chr(k % 256)))  # https://stackoverflow.com/questions/14494101/using-other-keys-for-the-waitkey-function-of-opencv
+                # break
 
         print('coded fixation_id:\t', fixation_id)
 
-        fixation_id = 'unknown'
+        # fixation_id = 'unknown'
 
-        if next_frame == False: break
+        if next_frame is False:
+            break
 
 
 drawFixation(LOG_FILE)
